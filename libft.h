a8680d1049b1a1b2dba6c75cba5c95328330acaf
kfujita <kfujita@student.42tokyo.jp>
2022-04-11 22:55:33 +0900

    fix prototypes
    - strncmp
    - memcmp
    - strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:37:46 by tetsu             #+#    #+#             */
-/*   Updated: 2022/04/10 16:52:03 by kfujita          ###   ########.fr       */
+/*   Updated: 2022/04/11 20:27:19 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 
-int		ft_strncmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 int		ft_atoi(const char *str);
 
